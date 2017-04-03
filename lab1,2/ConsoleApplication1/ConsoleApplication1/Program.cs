4134b9c59463bae508489c6fe8b5f8b09687769d
Jan Pałucki <jan.palucki@gmail.com>
2017-04-03 13:52:16 +0200

    WCF Services and Libraries Introduction

---
+++ lab1,2/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Elo Mordy");
+            Console.Beep();
+            Console.Read();
+            Console.Beep();
+            Console.Read();
+            Console.Beep();
+            Console.Read();
+        }
+    }
+}
---
+++ lab1,2/WcfServiceLibrary1/WcfServiceLibrary1/Service1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WcfServiceLibrary1
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in both code and config file together.
+    [ServiceBehavior(InstanceContextMode = InstanceContextMode.PerCall)]
+    public class Service1 : IService1
+    {
+
+        public int GetMoneyFromImperium()
+        {
+            Random r = new Random();
+            return r.Next(3000, 5000);
+        }
+
+        public string GetData(int value)
+        {
+            return string.Format("You entered: {0}", value);
+        }
+
+        public CompositeType GetDataUsingDataContract(CompositeType composite)
+        {
+            if (composite == null)
+            {
+                throw new ArgumentNullException("composite");
+            }
+            if (composite.BoolValue)
+            {
+                composite.StringValue += "Suffix";
+            }
+            return composite;
+        }
+    }
+}
