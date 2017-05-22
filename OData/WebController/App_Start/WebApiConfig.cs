99fad455380b85def39c2e34278694f9e40d4ed2
Jan Pałucki <jan.palucki@gmail.com>
2017-05-22 15:21:26 +0200

    OData GameController

---
+++ OData/WebController/App_Start/WebApiConfig.cs
@@ -0,0 +1,41 @@
+﻿using Backend;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using System.Web.OData.Builder;
+using System.Web.OData.Extensions;
+
+namespace WebController
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API configuration and services
+
+            // Web API routes
+            /*
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+            */
+
+            ODataModelBuilder builder = new ODataConventionModelBuilder();
+
+            builder.EntitySet<Game>("Games");
+            builder.EntitySet<Store>("Stores");
+
+            builder.Function("Dalajlama").Returns<double>().Parameter<int>("Postal");
+
+            config.MapODataServiceRoute(
+                routeName: "ODataRoute",
+                routePrefix: null,
+                model: builder.GetEdmModel());
+        }
+    }
+}
---
+++ OData/WebController/Controllers/GamesController.cs
@@ -0,0 +1,90 @@
+﻿using Backend;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.OData;
+
+namespace WebController.Controllers
+{
+    public class GamesController : ODataController
+    {
+        GamesContext db = new GamesContext();
+
+        [EnableQuery]
+        public IQueryable<Game> Get()
+        {
+            return db.Games;
+        }
+
+        [EnableQuery]
+        public SingleResult<Game> Get([FromODataUri] int key)
+        {
+            IQueryable<Game> result = db.Games.Where(g => g.Id == key);
+            return SingleResult.Create(result);
+        }
+
+        public async Task<IHttpActionResult> Post(Game Game)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Games.Add(Game);
+            await db.SaveChangesAsync();
+            return Created(Game);
+        }
+
+        public async Task<IHttpActionResult> Put([FromODataUri] int key, Game Game)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if(key != Game.Id)
+            {
+                return BadRequest();
+            }
+            db.Entry(Game).State = System.Data.Entity.EntityState.Modified;
+
+            try
+            {
+                await db.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!GameExists(key))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+            return Updated(Game);
+        }
+
+        public async Task<IHttpActionResult> Delete([FromODataUri] int key)
+        {
+            var game = await db.Games.FindAsync(key);
+            if (game == null)
+                return NotFound();
+
+            db.Games.Remove(game);
+            await db.SaveChangesAsync();
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        private bool GameExists(int key)
+        {
+            return db.Games.Any(g => g.Id == key);
+        }
+    }
+}
